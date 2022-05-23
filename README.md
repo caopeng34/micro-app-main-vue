dd5acd392b80e1882a719073bdb0370d19e84d80
caop <2510154937@qq.com>
2022-05-23 14:13:37 +0800

    微前端设置

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # micro-app-main-vue
 
+该应用为基座应用。
+
+
+
+引用的公共子模块（submodules\micro-app-common-vue）不可有node_modules文件夹，否则，引用公共子模块中的的代码使用的import Vue from 'vue'不是基座应用的而是公共子模块的。
--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import "./plugins/vant";
 import "microcommon/src/assets/less/index.less";
 import "microcommon/src/plugins/http.js";  // 里面含vue-ls引入
 import "microcommon/src/mixins/index.js"
+import GLoad from "microcommon/src/components/global-load/index"
+Vue.use(GLoad)
 
 Vue.config.productionTip = false
 
@@ -24,12 +26,14 @@ microApp.start({
     // 全局监听会在每个应用的生命周期执行时都会触发。
     lifeCycles: {
         created(e) {
+            Vue.prototype.$gload.gload('')  // 显示加载中
             console.log('micro-app:global:created')
         },
         beforemount(e) {
             console.log('micro-app:global:beforemount')
         },
         mounted(e) {
+            Vue.prototype.$gload.gload(false)  // 关闭加载中
             console.log('micro-app:global:mounted')
         },
         unmount(e) {
